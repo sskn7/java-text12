87a137cca5750358d5da618575f0c5664ffea1cf
kana sasazawa <kana.yellow7@gmail.com>
2024-12-25 23:36:27 +0900

    ループ文

---
+++ forbunnokiso.java
@@ -0,0 +1,47 @@
+
+public class forbunnokiso {
+
+	public static void main(String[] args) {
+		// for文の基礎　ループ文
+		//１から3までのループ
+		for (int i = 0;i<3;i++) {
+			System.out.println(i);
+		}
+		
+		//１から9未満
+		for (int a=1;a < 10;a++) {
+			System.out.println(a);
+		}
+		
+		//３から10未満
+		for (int b=3; b<10;b++) {
+			System.out.println(b);
+		}
+		
+		//３から１０まで
+		for (int c=3;c<=10;c++) {
+			System.out.println(c);
+		}
+		
+		//１０から6まで1づつ減る
+		for (int d =10;d>5;d--) {
+			System.out.println(d);
+		}
+		
+		//10から５まで1づつ減る
+		for (int e=10;e>=5;e--) {
+			System.out.println(e);
+		}
+		
+		//２づつ増える
+		for (int f=1;f<=10;f+=2) {
+			System.out.println(f);
+		}
+		
+		//２づつ減る
+		for (int h=1;h>=10;h-=2) {
+			System.out.println(h);
+		}
+	}
+
+}
